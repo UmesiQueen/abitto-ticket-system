9aa6959745895bea013f05cf24b0ed9aa532554e
Queen Umesi <queenumesi01@gmail.com>
2024-04-30 15:47:05 +0100

    Add responsiveness to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+// import MapIcon from "@mui/icons-material/Map";
+import { Twirl as Hamburger } from "hamburger-react";
+import React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import classNames from "classnames";
+
 function App() {
   return (
     <div className="relative overflow-hidden font-poppins">
@@ -5,10 +14,13 @@ function App() {
 
       <main>
         {/* Hero section */}
-        <section className="bg-hero-pattern h-screen w-screen bg-cover bg-no-repeat bg-center flex items-center relative ">
+        <section
+          id="home"
+          className="bg-hero-pattern h-screen w-screen bg-cover bg-no-repeat bg-center flex items-center relative "
+        >
           <div className="bg-black/40 w-full h-full absolute " />
-          <div className="w-[440px] pl-20 space-y-10 z-[1]">
-            <p className="font-semibold text-[40px] text-white">
+          <div className="w-[300px] md:w-[440px] pl-5 md:pl-20 space-y-10 z-[1]">
+            <p className="font-semibold text-2xl md:text-[40px] text-white md:leading-10">
               The Easiest & Safest way to travel within Nigeria & Africa
             </p>
             <button className="border-2 border-green-500 text-green-500 py-3 px-6 font-semibold text-sm ">
@@ -17,19 +29,23 @@ function App() {
           </div>
         </section>
         {/* About us  */}
-        <section className="p-20 px-44 bg-blue-50">
-          <div className="flex justify-between flex-1">
-            <div className="w-1/2">
+        <section className="p-5 md:py-20 md:px-44 bg-blue-50" id="about-us">
+          <div className="flex flex-col md:flex-row justify-between flex-1 ">
+            <div className="md:w-1/2 mb-10 md:mb-0 self-center">
               <img
                 src="https://i.ibb.co/8BLhcJw/about.jpg"
                 alt="about-us"
-                width={350}
-                height={420}
-                className=" -rotate-6 "
+                width={100}
+                height={100}
+                className=" md:-rotate-6 w-full md:w-[350px] h-[400px] md:h-[400px]"
               />
             </div>
-            <div className="w-1/2 space-y-5">
-              <h2 className="uppercase font-medium">About us</h2>
+            <div className="md:w-1/2 space-y-5">
+              <div className="flex items-center">
+                <span className=" h-3 w-3 mr-3 rounded-full bg-blue-500 " />
+                <h2 className="uppercase font-medium">About us</h2>
+              </div>
+
               <p className="font-medium text-2xl ">
                 We offer comprehensive range of stand-alone and integrated
                 services on time and on budget.
@@ -48,16 +64,21 @@ function App() {
           </div>
         </section>
         {/* Contact us  */}
-        <section className="p-20 px-44">
-          <div className="flex justify-between flex-1">
-            <div className="w-1/2 flex flex-col items-center space-y-5 ">
+        <section className="py-10 px-5 md:py-20 md:px-44" id="contact-us">
+          <div className="flex flex-col md:flex-row justify-between flex-1">
+            <div className="md:w-1/2 flex flex-col md:items-center space-y-5 mb-16 ">
+              <div className="flex items-center md:hidden">
+                <span className=" h-3 w-3 mr-3 rounded-full bg-blue-500 " />
+                <h2 className="uppercase font-medium">Our Location</h2>
+              </div>
               <img
                 src="https://i.ibb.co/z40PwNW/map.png"
                 alt="contact-us"
                 width={350}
                 height={420}
+                className="self-center"
               />
-              <ul className="w-[350px] px-5  text-lg [&_li]:flex [&_li]:gap-3 [&_li]:items-center space-y-5 ">
+              <ul className="md:w-[350px] px-5  text-lg [&_li]:flex [&_li]:gap-3 [&_li]:items-center space-y-5 ">
                 <li>
                   <img
                     src="/src/assets/icons/location.svg"
@@ -81,8 +102,11 @@ function App() {
                 </li>
               </ul>
             </div>
-            <div className="w-1/2 space-y-5">
-              <h2 className="uppercase font-medium">Contact us</h2>
+            <div className="md:w-1/2 space-y-5">
+              <div className="flex items-center">
+                <span className=" h-3 w-3 mr-3 rounded-full bg-blue-500 " />
+                <h2 className="uppercase font-medium">Contact us</h2>
+              </div>
               <form className="flex flex-col gap-y-5 *:bg-[#EEEEEE] *:p-2">
                 <input type="text" placeholder="Name" />
                 <input type="email" placeholder="Email address" />
@@ -99,11 +123,11 @@ function App() {
           </div>
         </section>
         {/* Extra */}
-        <section className="w-full h-[300px] flex flex-row-reverse items-center bg-boat bg-cover bg-no-repeat bg-center relative text-white">
+        <section className="w-full h-[300px] flex md:flex-row-reverse items-center bg-boat bg-cover bg-no-repeat bg-center relative text-white">
           <div className="w-full h-full bg-gradient-to-b from-white/0 from-50% to-blue-500/40 absolute z-[1]" />
           <div className="bg-black/80 w-full h-full absolute" />
-          <div className="w-1/2 z-[1]">
-            <p className="font-semibold text-2xl mb-8 ">
+          <div className="w-1/2 z-[1] pl-5 md:pl-0">
+            <p className="font-semibold text-2xl mb-8">
               Get started on your <br /> journey with us today!
             </p>
             <button className="bg-green-500 py-3 px-6 font-semibold text-sm">
@@ -113,82 +137,7 @@ function App() {
         </section>
       </main>
 
-      <footer className="bg-[#111111] p-10 px-20 text-white ">
-        <div className=" flex justify-between">
-          <div className=" space-y-16">
-            <img
-              src="https://i.ibb.co/17zsqj1/logo2.png"
-              alt="logo"
-              width={176}
-              height={60}
-            />
-            <ul className="flex items-center gap-4">
-              <li>
-                <img
-                  src="/src/assets/icons/Instagram.svg"
-                  alt="instagram"
-                  width={20}
-                  height={20}
-                />
-              </li>
-              <li>
-                <img
-                  src="/src/assets/icons/Facebook.svg"
-                  alt="facebook"
-                  width={10}
-                  height={10}
-                />
-              </li>
-              <li>
-                <img
-                  src="/src/assets/icons/Twitter.svg"
-                  alt="twitter"
-                  width={20}
-                  height={20}
-                />
-              </li>
-              <li>
-                <img
-                  src="/src/assets/icons/Linkedin.svg"
-                  alt="linkedin"
-                  width={20}
-                  height={20}
-                />
-              </li>
-            </ul>
-          </div>
-          <div>
-            <ul className="flex gap-x-28 [&_a]:block [&_a]:mb-2 [&_a]:text-sm [&_a]:font-normal [&_h3]:font-medium [&_h3]:text-2xl [&_h3]:mb-5">
-              <li>
-                <h3>Company</h3>
-                <a>About Us</a>
-                <a>Terms of Service</a>
-              </li>
-              <li>
-                <h3>Resources</h3>
-                <a>FAQ</a>
-                <a>Career</a>
-                <a>Videos</a>
-              </li>
-              <li>
-                <h3>Contact Us</h3>
-                <a>hi@abitto.com</a>
-                <a>Twitter Support</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <hr className="my-7" />
-        <div className="flex justify-between ">
-          <p>© 2023 Hello, Abitto</p>
-
-          <ul className="flex gap-8">
-            <li>Privacy</li>
-            <li>Terms of Agreement</li>
-            <li>Licenses</li>
-          </ul>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
@@ -196,22 +145,181 @@ function App() {
 export default App;
 
 const Navbar = () => {
+  const [isOpen, setOpen] = React.useState(false);
+
   return (
-    <nav className="absolute right-0 left-0 px-20 py-2 text-white flex items-center justify-between backdrop-blur-sm bg-[#1111111F] z-[1]">
+    <nav className="absolute right-0 left-0 px-5 md:px-20 py-2 text-white flex items-center justify-between backdrop-blur-sm bg-[#1111111F] z-[1]">
       <img
         alt="logo"
         src="https://i.ibb.co/17zsqj1/logo2.png"
         width={176}
         height={60}
       />
-      <ul className="flex *:uppercase *:font-semibold gap-x-3">
-        <li>home</li>
-        <li>about us</li>
-        <li>contact us</li>
+
+      <div className="md:hidden">
+        <Hamburger size={20} toggled={isOpen} toggle={setOpen} />
+      </div>
+
+      <ul
+        className={classNames(
+          "top-[77px] md:top-0 right-0 left-0 md:relative flex flex-col md:flex-row  *:uppercase *:font-semibold gap-x-3 bg-inherit md:bg-transparent text-center *:py-2 ",
+          !isOpen ? "hidden" : "absolute"
+        )}
+      >
+        <li className="hover:bg-gray-500/40">
+          <a href="#home">home</a>
+        </li>
+        <li className="hover:bg-gray-500/40">
+          <a href="#about-us">about us</a>
+        </li>
+        <li className="hover:bg-gray-500/40">
+          <a href="#contact-us">contact us</a>
+        </li>
       </ul>
-      <button className="bg-green-500 py-3 px-6 font-semibold text-sm">
+
+      <button className="hidden md:block bg-green-500 py-3 px-6 font-semibold text-sm">
         Get Started
       </button>
     </nav>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#111111] px-5 py-10 md:px-20 text-white ">
+      <div className=" md:flex justify-between ">
+        <div>
+          <img
+            src="https://i.ibb.co/17zsqj1/logo2.png"
+            alt="logo"
+            width={176}
+            height={60}
+          />
+        </div>
+
+        <ul className="hidden md:flex gap-x-28 [&_a]:block [&_a]:mb-2 [&_a]:text-sm [&_a]:font-normal [&_h3]:font-medium [&_h3]:text-2xl [&_h3]:mb-5">
+          <li>
+            <h3>Company</h3>
+            <a>About Us</a>
+            <a>Terms of Service</a>
+          </li>
+          <li>
+            <h3>Resources</h3>
+            <a>FAQ</a>
+            <a>Career</a>
+            <a>Videos</a>
+          </li>
+          <li>
+            <h3>Contact Us</h3>
+            <a>hi@abitto.com</a>
+            <a>Twitter Support</a>
+          </li>
+        </ul>
+
+        <ul className="md:hidden mt-10 mb-20 [&_a]:block [&_a]:mb-2 [&_a]:text-sm [&_a]:font-normal [&_h3]:font-medium [&_h3]:text-2xl [&_h3]:mb-5">
+          <li>
+            <Accordion
+              sx={{
+                background: "transparent",
+                color: "white",
+              }}
+            >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+                sx={{ padding: "0" }}
+                aria-controls="panel1-content"
+                id="panel1-header"
+              >
+                Company
+              </AccordionSummary>
+              <AccordionDetails>
+                <a>About Us</a>
+                <a>Terms of Service</a>
+              </AccordionDetails>
+            </Accordion>
+          </li>
+          <li>
+            <Accordion sx={{ background: "transparent", color: "white" }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+                sx={{ padding: "0" }}
+                aria-controls="panel2-content"
+                id="panel2-header"
+              >
+                Resources
+              </AccordionSummary>
+              <AccordionDetails>
+                <a>FAQ</a>
+                <a>Career</a>
+                <a>Videos</a>
+              </AccordionDetails>
+            </Accordion>
+          </li>
+          <li>
+            <Accordion sx={{ background: "transparent", color: "white" }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+                sx={{ padding: "0" }}
+                aria-controls="panel3-content"
+                id="panel3-header"
+              >
+                Contact Us
+              </AccordionSummary>
+              <AccordionDetails>
+                <a>hi@abitto.com</a>
+                <a>Twitter Support</a>
+              </AccordionDetails>
+            </Accordion>
+          </li>
+        </ul>
+      </div>
+
+      <ul className="flex items-center gap-4">
+        <li>
+          <img
+            src="/src/assets/icons/Instagram.svg"
+            alt="instagram"
+            width={20}
+            height={20}
+          />
+        </li>
+        <li>
+          <img
+            src="/src/assets/icons/Facebook.svg"
+            alt="facebook"
+            width={10}
+            height={10}
+          />
+        </li>
+        <li>
+          <img
+            src="/src/assets/icons/Twitter.svg"
+            alt="twitter"
+            width={20}
+            height={20}
+          />
+        </li>
+        <li>
+          <img
+            src="/src/assets/icons/Linkedin.svg"
+            alt="linkedin"
+            width={20}
+            height={20}
+          />
+        </li>
+      </ul>
+
+      <hr className="my-7" />
+
+      <div className="md:flex justify-between space-y-4 md:space-y-0">
+        <p>© 2023 Hello, Abitto</p>
+
+        <ul className="md:flex gap-8 space-y-4 md:space-y-0">
+          <li>Privacy</li>
+          <li>Terms of Agreement</li>
+          <li>Licenses</li>
+        </ul>
+      </div>
+    </footer>
   );
 };
