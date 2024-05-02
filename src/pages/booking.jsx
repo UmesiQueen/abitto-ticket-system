9673cbcfc495a1648f900b7e6448814511ecaf4b
Queen Umesi <queenumesi01@gmail.com>
2024-05-02 09:50:05 +0100

    Customize form fields

--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -1,11 +1,19 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
-import { styled } from "@mui/material/styles";
+// import { styled } from "@mui/material/styles";
 import classNames from "classnames";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import dayjs from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 
 const Booking = () => {
   const [value, setValue] = React.useState("1");
@@ -46,10 +54,10 @@ const Booking = () => {
               </TabList>
             </Box>
             <TabPanel value="1" sx={{ background: "#fff" }}>
-              <TicketForm tab="1" />
+              <BookingForm tab="1" />
             </TabPanel>
             <TabPanel value="2" sx={{ background: "#fff" }}>
-              <TicketForm tab="2" />
+              <BookingForm tab="2" />
             </TabPanel>
           </TabContext>
         </Box>
@@ -60,22 +68,22 @@ const Booking = () => {
 
 export default Booking;
 
-// eslint-disable-next-line react/prop-types
-const TicketForm = ({ tab }) => {
+const BookingForm = ({ tab }) => {
   return (
     <form className="font-poppins md:p-5 [&_label]:text-xs md:[&_label]:text-sm [&_label]:w-full [&_label]:flex [&_label]:gap-3 [&_label]:flex-col ">
       <div className="space-y-5">
         <h3 className="font-medium text-base ">Booking Details</h3>
-        <InputField
+
+        <SelectField
           label="Travelling From"
           placeholder="Select Departure Terminal"
-          type="text"
         />
-        <InputField
+
+        <SelectField
           label="Travelling To"
           placeholder="Select Arrival Terminal"
-          type="text"
         />
+
         {/* Departure Date */}
         <div
           className={classNames(
@@ -83,42 +91,14 @@ const TicketForm = ({ tab }) => {
             tab === "1" ? "flex-wrap md:flex-nowrap" : ""
           )}
         >
-          <label>
-            Date of Departure
-            <input
-              type="text"
-              className="bg-blue-50 p-3 border border-blue-500 font-normal text-xs  w-full "
-              placeholder="21/04/2024"
-            />
-          </label>
-          <label>
-            Time of Departure
-            <input
-              type="text"
-              className="bg-blue-50 p-3 border border-blue-500 font-normal text-xs w-full "
-              placeholder="08:00 AM"
-            />
-          </label>
+          <DateField label="Date of Departure" />
+          <TimeField label="Time of Departure" />
         </div>
         {/* Round Trip */}
         {tab === "2" && (
           <div className="flex gap-5">
-            <label>
-              Date of Return
-              <input
-                type="text"
-                className="bg-blue-50 p-3 border border-blue-500 font-normal text-xs  w-full"
-                placeholder="21/04/2024"
-              />
-            </label>
-            <label>
-              Time of Return
-              <input
-                type="text"
-                className="bg-blue-50 p-3 border border-blue-500 font-normal text-xs w-full "
-                placeholder="08:00 AM"
-              />
-            </label>
+            <DateField label="Date of Return" />
+            <TimeField label="Time of Return" />
           </div>
         )}
         <div className="flex gap-5">
@@ -142,7 +122,7 @@ const TicketForm = ({ tab }) => {
           <InputField
             label="Phone Number"
             placeholder="(+234) XXXX XXX XXX"
-            type="number"
+            type="tel"
           />
         </div>
       </div>
@@ -157,13 +137,13 @@ const TicketForm = ({ tab }) => {
   );
 };
 
-// eslint-disable-next-line react/prop-types
 const InputField = ({ label, type, placeholder }) => {
   return (
     <label className="text-sm w-full flex gap-3 flex-col ">
       {label}
       <input
         type={type}
+        min={0}
         className="bg-blue-50 p-3 border border-blue-500 font-normal text-xs w-full "
         placeholder={placeholder}
       />
@@ -171,23 +151,110 @@ const InputField = ({ label, type, placeholder }) => {
   );
 };
 
-const StyledTabList = styled((props) => (
-  <TabList
-    {...props}
-    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
-  />
-))({
-  "& .MuiTabs-indicator": {
-    display: "flex",
-    justifyContent: "center",
-    height: "100%",
-    backgroundColor: "3366cc",
-  },
-  "& .MuiTabs-indicatorSpan": {
-    width: "100%",
-    height: "15px",
-    borderRadius: "49% 49% 0% 0% / 88% 89% 11% 12%;",
-    marginTop: "auto",
-    backgroundColor: "#D9D9D91F",
-  },
-});
+const SelectField = ({ label, placeholder }) => {
+  const [departure, setDeparture] = React.useState("");
+
+  const handleChange = (event) => {
+    setDeparture(event.target.value);
+  };
+
+  return (
+    <label className="text-sm w-full flex gap-3 flex-col ">
+      {label}
+      <Select
+        id="demo-simple-select"
+        value={departure}
+        onChange={handleChange}
+        displayEmpty
+        renderValue={
+          departure !== ""
+            ? undefined
+            : () => (
+                <span className="noTranslate text-xs font-poppins text-[#9fa6b2]">
+                  {placeholder}
+                </span>
+              )
+        }
+        className="bg-blue-50 h-10 border border-blue-500 font-normal text-xs w-full !rounded-none"
+      >
+        <MenuItem value="calabar">Calabar</MenuItem>
+        <MenuItem value="uyo">Uyo</MenuItem>
+      </Select>
+    </label>
+  );
+};
+
+const DateField = ({ label }) => {
+  const [value, setValue] = React.useState(dayjs("21/04/2024"));
+
+  return (
+    <label className="text-sm w-full flex gap-3 flex-col ">
+      {label}
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+          sx={{
+            "& .MuiInputBase-root": {
+              height: "44px",
+              borderRadius: "0",
+              borderColor: "#3366CC",
+              borderWidth: "1px",
+              padding: "0 12px",
+              backgroundColor: "#ebf0fa",
+            },
+          }}
+          slotProps={{
+            textField: { variant: "standard" },
+          }}
+        />
+      </LocalizationProvider>
+    </label>
+  );
+};
+
+const TimeField = ({ label }) => {
+  return (
+    <label className="text-sm w-full flex gap-3 flex-col">
+      {label}
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <TimePicker
+          sx={{
+            "& .MuiInputBase-root": {
+              height: "44px",
+              borderRadius: "0",
+              borderColor: "#3366CC",
+              borderWidth: "1px",
+              padding: "0 12px",
+              backgroundColor: "#ebf0fa",
+            },
+          }}
+          slotProps={{
+            textField: { variant: "standard" },
+          }}
+        />
+      </LocalizationProvider>
+    </label>
+  );
+};
+
+// const StyledTabList = styled((props) => (
+//   <TabList
+//     {...props}
+//     TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+//   />
+// ))({
+//   "& .MuiTabs-indicator": {
+//     display: "flex",
+//     justifyContent: "center",
+//     height: "100%",
+//     backgroundColor: "3366cc",
+//   },
+//   "& .MuiTabs-indicatorSpan": {
+//     width: "100%",
+//     height: "15px",
+//     borderRadius: "49% 49% 0% 0% / 88% 89% 11% 12%;",
+//     marginTop: "auto",
+//     backgroundColor: "#D9D9D91F",
+//   },
+// });
