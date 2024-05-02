08bc1b1618a97130ba8b44c2fc5adbf44ca950e2
Queen Umesi <queenumesi01@gmail.com>
2024-05-02 14:57:13 +0100

    Add 404 erorr page

---
+++ src/pages/pageNotFound.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+const PageNotFound = () => {
+  return (
+    <div className="h-[70vh] bg-blue-950 flex flex-col justify-center items-center ">
+      <h1 className="font-bold text-4xl mb-10 text-slate-900 ">
+        Page Not Found.
+      </h1>
+      <button className=" bg-green-500 py-3 px-6 font-semibold text-sm  hover:bg-green-700 transition-all duration-100  ease-in-out hover:scale-[1.1] text-white mt-5">
+        <Link to={"/"}>Go Home</Link>
+      </button>
+    </div>
+  );
+};
+
+export default PageNotFound;
--- src/routes.jsx
+++ src/routes.jsx
@@ -7,12 +7,14 @@ import {
 import App from "./App";
 import Home from "./pages";
 import Booking from "./pages/booking";
+import PageNotFound from "./pages/pageNotFound";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Home />} />
       <Route path="/booking" element={<Booking />} />
+      <Route path="*" element={<PageNotFound />} />
     </Route>
   )
 );
