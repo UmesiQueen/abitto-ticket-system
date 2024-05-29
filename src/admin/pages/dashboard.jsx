18a5e6f0869fd72c83433378fc417d95d0b04ca0
Queen Umesi <queenumesi01@gmail.com>
2024-05-29 11:36:49 +0100

    Chart template

--- src/admin/pages/dashboard.jsx
+++ src/admin/pages/dashboard.jsx
@@ -14,7 +14,9 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
+import { BarChart } from "@tremor/react";
 
 const users = [
   {
@@ -52,14 +54,80 @@ const users = [
   },
 ];
 
+const chartDataDemo = [
+  {
+    date: "Jan",
+    "first half": 1488,
+    "second half": 300,
+  },
+  {
+    date: "Feb",
+    "first half": 1445,
+    "second half": 900,
+  },
+  {
+    date: "March",
+    "first half": 743,
+    "second half": 1000,
+  },
+  {
+    date: "April",
+    "first half": 281,
+    "second half": 1900,
+  },
+  {
+    date: "May",
+    "first half": 1501,
+    "second half": 1700,
+  },
+  {
+    date: "Jun",
+    "first half": 2302,
+    "second half": 900,
+  },
+  {
+    date: "July",
+    "first half": 1098,
+    "second half": 970,
+  },
+  {
+    date: "Aug",
+    "first half": 980,
+    "second half": 2300,
+  },
+  {
+    date: "Sept",
+    "first half": 1998,
+    "second half": 1000,
+  },
+  {
+    date: "Oct",
+    "first half": 2188,
+    "second half": 1300,
+  },
+  {
+    date: "Nov",
+    "first half": 98,
+    "second half": 2300,
+  },
+  {
+    date: "Dec",
+    "first half": 1098,
+    "second half": 300,
+  },
+];
+
 const Dashboard = () => {
+  const dataFormatter = (number) =>
+    Intl.NumberFormat("us").format(number).toString();
+
   return (
     <div className="">
-      <h1 className="text-base  font-semibold">Dashboard Overview</h1>{" "}
+      <h1 className="text-lg font-semibold">Dashboard Overview</h1>
       <div className="mt-8 flex flex-col gap-5 ">
         <div className="bg-white rounded-lg p-5 ">
-          <ul className="border rounded-lg  p-5 flex justify-between ">
-            <li className="flex items-center gap-3 w-56 ">
+          <ul className="border rounded-lg  p-5 flex justify-between items-center h-[100px] ">
+            <li className="flex items-center gap-3 ">
               <div className="rounded-lg bg-blue-50 p-2">
                 <WalletIcon />
               </div>
@@ -70,7 +138,8 @@ const Dashboard = () => {
                 </p>
               </div>
             </li>
-            <li className="flex items-center gap-3 w-56 ">
+            <Separator orientation={"vertical"} />
+            <li className="flex items-center gap-3 ">
               <div className="rounded-lg bg-blue-50 p-2">
                 <UserGroupIcon />
               </div>
@@ -81,7 +150,8 @@ const Dashboard = () => {
                 </p>
               </div>
             </li>
-            <li className="flex items-center gap-3 w-56 ">
+            <Separator orientation={"vertical"} />
+            <li className="flex items-center gap-3 ">
               <div className="rounded-lg bg-blue-50 p-2">
                 <FerryBoatIcon />
               </div>
@@ -92,6 +162,7 @@ const Dashboard = () => {
                 </p>
               </div>
             </li>
+            <Separator orientation={"vertical"} />
             <li className="flex items-center gap-3 w-56 ">
               <div className="rounded-lg bg-blue-50 p-2">
                 <span className="text-blue-500">
@@ -110,11 +181,53 @@ const Dashboard = () => {
 
         {/* charts */}
         <div className="flex gap-5">
-          <div className="bg-white rounded-lg basis-9/12 ">
-            <div className="bg-[url(https://i.ibb.co/sCr6jMM/Chart.png)] bg-contain bg-no-repeat bg-center h-[400px]" />
+          <div className="bg-white rounded-lg basis-9/12 p-5 ">
+            {/* <div className="bg-[url(https://i.ibb.co/sCr6jMM/Chart.png)] bg-contain bg-no-repeat bg-center h-[400px]" /> */}
+            <div className="flex justify-between mb-7">
+              <hgroup className="flex gap-1 items-center">
+                <h3 className="font-semibold">Sales Revenue</h3>
+                <span>(N1,303,030)</span>
+              </hgroup>
+
+              <div className="flex items-center gap-2">
+                <button className="rounded-lg p-2 border">13, Feb 2024</button>
+                <p>to</p>
+                <button className="rounded-lg p-2 border">14, Feb 2024</button>
+              </div>
+            </div>
+            <BarChart
+              data={chartDataDemo}
+              index="date"
+              categories={["first half", "second half"]}
+              colors={["blue"]}
+              valueFormatter={dataFormatter}
+              yAxisWidth={48}
+              onValueChange={(v) => console.log(v)}
+              showLegend={false}
+            />
           </div>
-          <div className="bg-white rounded-lg basis-3/12">
-            <div className="bg-[url(https://i.ibb.co/km0dbXQ/today-booking.png)] bg-contain bg-no-repeat bg-center h-[400px]" />
+          <div className="bg-white rounded-lg basis-3/12 p-5">
+            {/* <div className="bg-[url(https://i.ibb.co/km0dbXQ/today-booking.png)] bg-contain bg-no-repeat bg-center h-[400px]" /> */}
+            <div className="rounded-lg border h-full p-5 space-y-5">
+              <h3 className="font-semibold">Today Booking</h3>
+              <div className="relative">
+                <div className="arc" />
+                <div className="w-fit absolute top-24 left-1/2 right-1/2 -translate-x-1/2 space-y-3">
+                  <p className="text-center flex flex-col">
+                    <b className=" text-3xl font-semibold">120</b>
+                    <span className="text-[#7F7F7F] text-base">
+                      Total Bookings
+                    </span>
+                  </p>
+                  <p className=" !mt-16 text-[#7F7F7F] before:content-[''] before:px-3 before:rounded-full before:bg-blue-500 before:mr-3 text-nowrap">
+                    0 Offline Booking
+                  </p>
+                  <p className=" text-[#7F7F7F] before:content-[''] before:px-3 before:rounded-full before:bg-green-500 before:mr-3 text-nowrap">
+                    120 Online Booking
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -122,8 +235,8 @@ const Dashboard = () => {
         <div className="bg-white rounded-lg p-5 mb-5 ">
           <div className="border rounded-lg ">
             <div className="flex items-center gap-5 mb-5 p-5 border-b">
-              <h2 className=" font-semibold text-sm ">Latest Booking List</h2>
-              <div className="rounded-lg border px-4 p-2 cursor-pointer flex items-center gap-3 ml-auto">
+              <h3 className="font-semibold">Latest Booking List</h3>
+              <div className="rounded-lg border px-4 p-2 cursor-pointer flex items-center gap-2 ml-auto">
                 <span>
                   <FilterIcon />
                 </span>
