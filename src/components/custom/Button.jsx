ea0d359232baaadb5a63f7508b767cd5b72997cd
Queen Umesi <queenumesi01@gmail.com>
2024-06-20 09:56:54 +0100

    Add disabled style to custom button

--- src/components/custom/Button.jsx
+++ src/components/custom/Button.jsx
@@ -4,16 +4,17 @@ import PropTypes from "prop-types";
 const Button = ({
   text = "Button",
   loading = false,
+  disabled = loading,
   className,
   type = "button",
   onClick,
 }) => {
   return (
     <button
-      disabled={loading}
+      disabled={disabled}
       type={type}
       onClick={onClick}
-      className={`bg-blue-500 py-3 max-w-full font-semibold text-sm hover:bg-blue-700 hover:text-stone-200 transition-all duration-150 ease-in-out text-white flex justify-center rounded-lg ${className}`}
+      className={`bg-blue-500 py-3 max-w-full font-semibold text-sm hover:bg-blue-700 hover:text-stone-200 transition-all duration-150 ease-in-out text-white flex justify-center rounded-lg  disabled:pointer-events-none disabled:hover:text-white disabled:bg-[#C2C2C2] ${className}`}
     >
       {loading ? (
         <ClipLoader
@@ -35,6 +36,7 @@ Button.propTypes = {
   className: PropTypes.string,
   type: PropTypes.string,
   onClick: PropTypes.func,
+  disabled: PropTypes.bool,
 };
 
 export default Button;
