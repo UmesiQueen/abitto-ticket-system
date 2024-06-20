655370b786e8264657f46fb3c92c84790614b8a7
Queen Umesi <queenumesi01@gmail.com>
2024-06-20 10:16:15 +0100

    delete overlay and seat selection modal

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Loader from "@/components/animation/Loader";
 import { Toaster } from "sonner";
 import GlobalContext from "./hooks/GlobalContext";
 import "./index.css";
-import Overlays from "@/components/custom/Overlays";
 
 const App = () => {
   const pathname = useLocation();
@@ -19,7 +18,6 @@ const App = () => {
         <Outlet />
         <Loader />
         <Toaster position="top-center" expand={true} richColors />
-        <Overlays />
       </div>
     </GlobalContext>
   );
