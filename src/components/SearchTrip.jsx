9e6a8f18d18fce24b0e440b4ceeec8b8c3963ff9
Queen Umesi <queenumesi01@gmail.com>
2024-07-13 15:51:50 +0100

    add responsiveness to component

--- src/components/SearchTrip.jsx
+++ src/components/SearchTrip.jsx
@@ -74,7 +74,11 @@ const SearchTrip = () => {
         bookingDetails: {
           ...prev.bookingDetails,
           total_passengers,
-          amount: totalCost,
+          ticket_cost: totalCost,
+          departure_time: selectedTrip.departure.time,
+          ...(trip_type == "Round Trip" && {
+            return_time: selectedTrip.return.time,
+          }),
         },
       }));
       onNextClick();
@@ -82,14 +86,24 @@ const SearchTrip = () => {
     }, 650);
   };
 
+  const handleReturn = () => {
+    onPrevClick();
+    setSelectedTrip({
+      departure: {},
+      return: {},
+    });
+  };
+
   return (
-    <section className="space-y-10 min-h-56">
-      <div className="flex gap-5 justify-between items-center">
+    <section className="space-y-10 px-3 md:px-0">
+      <div className="flex flex-col-reverse md:flex-row gap-5 justify-between md:items-center">
         <hgroup>
-          <h2 className="font-semibold text-lg">Select Departure Trip</h2>
+          <h2 className="font-semibold text-sm md:text-lg">
+            Select Departure Trip
+          </h2>
           <p className="text-sm">Choose an option to continue</p>
         </hgroup>
-        <div className="text-xs text-gray-500 bg-gray-200 h-12 p-1 flex gap-2 rounded-lg *:inline-flex *:items-center *:px-2 [&>*.active]:border-blue-500 [&>*.active]:border-2 [&>*.active]:font-semibold [&>*.active]:text-blue-500 [&>*.active]:rounded-lg">
+        <div className=" self-end text-xs text-gray-500 bg-gray-200 h-12 p-1 flex gap-2 rounded-lg *:inline-flex *:items-center *:px-2 [&>*.active]:border-blue-500 [&>*.active]:border-2 [&>*.active]:font-semibold [&>*.active]:text-blue-500 *:rounded-lg">
           <p className={trip_type == "One-Way Trip" ? "active" : ""}>One-Way</p>
           <p className={trip_type == "Round Trip" ? "active" : ""}>
             Round Trip
@@ -107,31 +121,31 @@ const SearchTrip = () => {
               <div
                 key={item.trip_code}
                 data-state={isActive ? "active" : ""}
-                className=" relative flex h-32 *:rounded-lg *:bg-white [&>*]:data-[state=active]:bg-blue-50 [&>*]:data-[state=active]:shadow-sm "
+                onClick={() => {
+                  handleCheck("departure", !isActive, item);
+                }}
+                className="grid overflow-hidden grid-cols-3 md:grid-cols-7 rounded-lg border border-gray-400 md:border-none divide-y divide-solid md:divide-y-0  min-h-32 *:bg-white [&>*]:data-[state=active]:bg-blue-50 data-[state=active]:shadow-lg *:py-2 transition-all duration-150 ease-in-out"
               >
-                <button
-                  className="absolute top-0 left-0 w-full h-full !bg-transparent"
-                  onClick={() => {
-                    handleCheck("departure", !isActive, item);
-                  }}
-                />
-                <ul className="px-8 flex-grow flex justify-between items-center">
-                  <li className="space-y-1">
+                <ul className="px-5 md:px-8 col-span-3 md:col-span-4 flex-grow flex gap-3 justify-between items-center rounded-t-lg md:rounded-lg">
+                  <li className="space-y-1 basis-2/5">
                     <p className="font-bold">{item.time}</p>
                     <p className="text-gray-500 text-sm">{item.departure}</p>
                     <p className="text-gray-500 text-sm">{item.date}</p>
                   </li>
-                  <li className="text-gray-500 space-y-1">
-                    <p className="text-xs text-center">1 hour</p>
-                    <div className="inline-flex items-center">
+                  <li className="text-gray-500 space-y-1 text-center basis-1/5">
+                    <p className="text-xs">1 hour</p>
+                    <div className="hidden md:inline-flex items-center">
                       ---
                       <span className="rounded-full border border-gray-200 w-9 h-9 inline-flex justify-center items-center">
                         <BoatIcon />
                       </span>
                       ---
                     </div>
+                    <span className="md:hidden rounded-full border border-gray-200 p-1 inline-flex justify-center items-center">
+                      <BoatIcon />
+                    </span>
                   </li>
-                  <li className="space-y-1">
+                  <li className="space-y-1 text-right basis-2/5">
                     <p className="font-bold">
                       {format(getArrivalDateTime(item.time, item.date), "p")}
                     </p>
@@ -141,7 +155,7 @@ const SearchTrip = () => {
                     </p>
                   </li>
                 </ul>
-                <div className="px-6 min-w-36 flex flex-col justify-center border-l-2 border-gray-400 border-dashed">
+                <div className="px-6 col-span-2 md:col-span-1 flex flex-col justify-center items-center md:rounded-lg md:border-l-2 border-gray-400">
                   <p className="text-sm">Price(NGN)</p>
                   <p className="md:text-lg font-semibold">
                     {formatValue({
@@ -150,11 +164,11 @@ const SearchTrip = () => {
                     })}
                   </p>
                 </div>
-                <div className="px-6 min-w-36 flex flex-col justify-center border-l-2 border-gray-400 border-dashed">
+                <div className="px-6 col-span-2 md:col-span-1 col-start-1 flex flex-col justify-center rounded-bl-lg  md:rounded-lg items-center md:border-l-2 border-gray-400">
                   <p className="text-sm">Status</p>
                   <p className="font-semibold uppercase">Available</p>
                 </div>
-                <div className="px-6 min-w-36 flex items-center justify-center border-l-2 border-gray-400 border-dashed">
+                <div className="px-6 row-start-2 md:row-row-start-1 row-span-2 md:row-span-1 col-start-3 md:col-start-7 rounded-br-lg md:rounded-lg flex items-center justify-center border-l md:border-l-2  border-gray-400">
                   <Checkbox
                     tabIndex={-1}
                     className="data-[state=checked]:bg-blue-500 data-[state=checked]:border-blue-500"
@@ -185,31 +199,31 @@ const SearchTrip = () => {
                 <div
                   key={item.trip_code}
                   data-state={isActive ? "active" : ""}
-                  className=" relative flex h-32 *:rounded-lg *:bg-white [&>*]:data-[state=active]:bg-blue-50 [&>*]:data-[state=active]:shadow-sm "
+                  onClick={() => {
+                    handleCheck("return", !isActive, item);
+                  }}
+                  className="grid overflow-hidden grid-cols-3 md:grid-cols-7 rounded-lg border border-gray-400 md:border-none divide-y divide-solid md:divide-y-0  min-h-32 *:bg-white [&>*]:data-[state=active]:bg-blue-50 data-[state=active]:shadow-lg *:py-2 transition-all duration-150 ease-in-out"
                 >
-                  <button
-                    className="absolute top-0 left-0 w-full h-full !bg-transparent"
-                    onClick={() => {
-                      handleCheck("return", !isActive, item);
-                    }}
-                  />
-                  <ul className="px-8 flex-grow flex justify-between items-center">
-                    <li className="space-y-1">
+                  <ul className="px-5 md:px-8 col-span-3 md:col-span-4 flex-grow flex gap-3 justify-between items-center rounded-t-lg md:rounded-lg">
+                    <li className="space-y-1 basis-2/5">
                       <p className="font-bold">{item.time}</p>
                       <p className="text-gray-500 text-sm">{item.departure}</p>
                       <p className="text-gray-500 text-sm">{item.date}</p>
                     </li>
-                    <li className="text-gray-500 space-y-1">
-                      <p className="text-xs text-center">1 hour</p>
-                      <div className="inline-flex items-center">
+                    <li className="text-gray-500 space-y-1 text-center basis-1/5">
+                      <p className="text-xs">1 hour</p>
+                      <div className="hidden md:inline-flex items-center">
                         ---
                         <span className="rounded-full border border-gray-200 w-9 h-9 inline-flex justify-center items-center">
                           <BoatIcon />
                         </span>
                         ---
                       </div>
+                      <span className="md:hidden rounded-full border border-gray-200 p-1 inline-flex justify-center items-center">
+                        <BoatIcon />
+                      </span>
                     </li>
-                    <li className="space-y-1">
+                    <li className="space-y-1 text-right basis-2/5">
                       <p className="font-bold">
                         {format(getArrivalDateTime(item.time, item.date), "p")}
                       </p>
@@ -219,7 +233,7 @@ const SearchTrip = () => {
                       </p>
                     </li>
                   </ul>
-                  <div className="px-6 min-w-36 flex flex-col justify-center border-l-2 border-gray-400 border-dashed">
+                  <div className="px-6 col-span-2 md:col-span-1 flex flex-col justify-center items-center md:rounded-lg md:border-l-2 border-gray-400">
                     <p className="text-sm">Price(NGN)</p>
                     <p className="md:text-lg font-semibold">
                       {formatValue({
@@ -228,11 +242,11 @@ const SearchTrip = () => {
                       })}
                     </p>
                   </div>
-                  <div className="px-6 min-w-36 flex flex-col justify-center border-l-2 border-gray-400 border-dashed">
+                  <div className="px-6 col-span-2 md:col-span-1 col-start-1 flex flex-col justify-center rounded-bl-lg  md:rounded-lg items-center md:border-l-2 border-gray-400">
                     <p className="text-sm">Status</p>
                     <p className="font-semibold uppercase">Available</p>
                   </div>
-                  <div className="px-6 min-w-36 flex items-center justify-center border-l-2 border-gray-400 border-dashed">
+                  <div className="px-6 row-start-2 md:row-row-start-1 row-span-2 md:row-span-1 col-start-3 md:col-start-7 rounded-br-lg md:rounded-lg flex items-center justify-center border-l md:border-l-2  border-gray-400">
                     <Checkbox
                       tabIndex={-1}
                       className="data-[state=checked]:bg-blue-500 data-[state=checked]:border-blue-500"
@@ -250,10 +264,12 @@ const SearchTrip = () => {
         </div>
       )}
 
-      <div className="flex h-28 *:rounded-lg *:bg-white *:px-8">
-        <div className=" flex-grow flex justify-between items-center gap-5">
-          <p className="font-semibold uppercase">Total Cost</p>
-          <p className="text-right">
+      <div className="grid grid-cols-1 md:grid-cols-7 gap-y-5 min-h-32">
+        <div className="md:col-span-4 flex-grow flex justify-between items-end rounded-t-lg rounded-lg  bg-white p-8 min-h-16">
+          <p className="font-semibold text-sm md:text-lg uppercase">
+            Total Cost
+          </p>
+          <p className="text-right md:text-xl">
             <strong>
               {formatValue({
                 value: String(totalCost),
@@ -261,45 +277,40 @@ const SearchTrip = () => {
               })}
             </strong>
             <span className="block text-sm">
+              {formatValue({
+                value: String(selectedTrip.departure?.ticket_cost ?? 0),
+                prefix: "₦",
+              })}
               {trip_type === "Round Trip" && (
                 <>
+                  {" + "}
                   {formatValue({
                     value: String(selectedTrip.return?.ticket_cost ?? 0),
                     prefix: "₦",
                   })}
-                  {" + "}
                 </>
-              )}
-              {formatValue({
-                value: String(selectedTrip.departure?.ticket_cost ?? 0),
-                prefix: "₦",
-              })}{" "}
+              )}{" "}
               x {total_passengers} Passenger(s)
             </span>
           </p>
         </div>
-        <div className="flex items-center justify-center gap-3 border-l-2 border-gray-400 border-dashed">
+        <div className="md:col-span-3 flex items-center justify-center gap-5 md:border-l-2 border-gray-400 rounded-t-lg md:rounded-lg  md:bg-white md:px-8">
           <Button
             text="Back"
             variant="outline"
-            className="p-5 w-44"
-            onClick={() => {
-              onPrevClick();
-              setSelectedTrip({
-                departure: {},
-                return: {},
-              });
-            }}
+            className="w-full basis-1/2"
+            onClick={handleReturn}
           />
           <div
             onClick={() => {
               if (isDisabled)
                 toast.info("Please select preferred trip(s) to proceed.");
             }}
+            className="w-full basis-1/2"
           >
             <Button
               text="Continue"
-              className="p-5 w-44"
+              className="w-full"
               disabled={isDisabled}
               onClick={handleSubmit}
             />
