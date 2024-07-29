6d2182b6c87878cf004c48c0087d05df2a50bebc
Queen <queenumesi01@gmail.com>
2024-07-29 08:03:35 +0100

    change time

--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -96,7 +96,7 @@ const BookingForm = ({ tab }) => {
     context: { roundTrip: tab === "Round Trip" ? true : false },
   });
 
-  const defaultTimeOptions = ["12:00 PM", "02:00 PM"];
+  const defaultTimeOptions = ["09:00 AM", "11:00 AM"];
 
   const { loading, setLoading } = React.useContext(GlobalCTX);
   const { setFormData } = React.useContext(BookingCTX);
@@ -114,8 +114,8 @@ const BookingForm = ({ tab }) => {
   }, [travel_from]);
 
   const resetTimeOptions = (travel_from) => {
-    const calabarTimeOptions = ["12:00 PM"];
-    const uyoTimeOptions = ["02:00 PM"];
+    const calabarTimeOptions = ["09:00 AM"];
+    const uyoTimeOptions = ["11:00 AM"];
 
     if (travel_from) {
       travel_from === "Nwaniba Timber Beach, Uyo"
