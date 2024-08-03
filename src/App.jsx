db97bc7d74d92a1c8e35a7f73d17a5b0e7cc3fe9
Queen Umesi <queenumesi01@gmail.com>
2024-08-03 09:42:24 +0100

    add padding to sonner toast

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,28 @@ import "./index.css";
 import ModalPortal from "./components/ModalPortal";
 
 const App = () => {
-  const pathname = useLocation();
+	const pathname = useLocation();
 
-  React.useEffect(() => {
-    if (window.scrollY !== 0) window.scrollTo({ top: 0, behavior: "smooth" });
-  }, [pathname]);
+	React.useEffect(() => {
+		if (window.scrollY !== 0) window.scrollTo({ top: 0, behavior: "smooth" });
+	}, [pathname]);
 
-  return (
-    <GlobalContext>
-      <div className="font-poppins overflow-hidden">
-        <Outlet />
-        <Toaster position="top-center" expand={true} richColors />
-        <ModalPortal />
-      </div>
-    </GlobalContext>
-  );
+	return (
+		<GlobalContext>
+			<div className="font-poppins overflow-hidden">
+				<Outlet />
+				<Toaster
+					position="top-center"
+					expand={true}
+					richColors
+					toastOptions={{
+						className: "p-2",
+					}}
+				/>
+				<ModalPortal />
+			</div>
+		</GlobalContext>
+	);
 };
 
 export default App;
