e702018d4a32ffbc9c3781a2a29e9dff7686942c
Queen Umesi <queenumesi01@gmail.com>
2024-08-06 03:37:48 +0100

    fix: make grid-cols-1 on mobile

--- src/components/PassengerDetails.jsx
+++ src/components/PassengerDetails.jsx
@@ -13,70 +13,70 @@ import SeatSelection from "@/components/SeatSelection";
 import { format } from "date-fns";
 
 const PassengerDetails = () => {
-  const { loading, setLoading, mountPortalModal } = React.useContext(GlobalCTX);
-  const { setChecked, isChecked, formData, setFormData } =
-    React.useContext(BookingCTX);
-  const { onPrevClick, onNextClick } = useStepper();
-  const adults_number = formData.bookingDetails?.adults_number;
+	const { loading, setLoading, mountPortalModal } = React.useContext(GlobalCTX);
+	const { setChecked, isChecked, formData, setFormData } =
+		React.useContext(BookingCTX);
+	const { onPrevClick, onNextClick } = useStepper();
+	const adults_number = formData.bookingDetails?.adults_number;
 
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm({
-    mode: "onChange",
-    resolver: yupResolver(passengerDetailsSchema),
-    context: { adultPassengers: adults_number, isChecked },
-    defaultValues: {
-      ...formData.passengerDetails,
-    },
-  });
+	const {
+		register,
+		handleSubmit,
+		setValue,
+		formState: { errors },
+	} = useForm({
+		mode: "onChange",
+		resolver: yupResolver(passengerDetailsSchema),
+		context: { adultPassengers: adults_number, isChecked },
+		defaultValues: {
+			...formData.passengerDetails,
+		},
+	});
 
-  const onSubmit = handleSubmit((formData_) => {
-    const formValues = {
-      ...(isChecked
-        ? {
-            passenger1_first_name: formData_.passenger1_first_name,
-            passenger1_surname: formData_.passenger1_surname,
-            passenger1_phone_number: formData_.passenger1_phone_number,
-            passenger1_email: formData_.passenger1_email,
-          }
-        : {
-            ...formData_,
-          }),
-    };
+	const onSubmit = handleSubmit((formData_) => {
+		const formValues = {
+			...(isChecked
+				? {
+						passenger1_first_name: formData_.passenger1_first_name,
+						passenger1_surname: formData_.passenger1_surname,
+						passenger1_phone_number: formData_.passenger1_phone_number,
+						passenger1_email: formData_.passenger1_email,
+				  }
+				: {
+						...formData_,
+				  }),
+		};
 
-    setLoading(true);
-    setTimeout(() => {
-      setFormData((prev) => ({
-        ...prev,
-        passengerDetails: formValues,
-      }));
-      setLoading(false);
-      onNextClick();
-    }, 650);
-  });
+		setLoading(true);
+		setTimeout(() => {
+			setFormData((prev) => ({
+				...prev,
+				passengerDetails: formValues,
+			}));
+			setLoading(false);
+			onNextClick();
+		}, 650);
+	});
 
-  const handleSeatSelection = (e) => {
-    const tab = e.target.name.split("_")[0];
-    mountPortalModal(<SeatSelection props={{ tab, setValue }} />);
-  };
+	const handleSeatSelection = (e) => {
+		const tab = e.target.name.split("_")[0];
+		mountPortalModal(<SeatSelection props={{ tab, setValue }} />);
+	};
 
-  const handleChange = (e) => {
-    const { value, name } = e.target;
-    setFormData((prev) => {
-      return {
-        ...prev,
-        passengerDetails: {
-          ...prev.passengerDetails,
-          [name]: value,
-        },
-      };
-    });
-  };
+	const handleChange = (e) => {
+		const { value, name } = e.target;
+		setFormData((prev) => {
+			return {
+				...prev,
+				passengerDetails: {
+					...prev.passengerDetails,
+					[name]: value,
+				},
+			};
+		});
+	};
 
-  return (
+	return (
 		<>
 			<div className="bg-blue-700 mx-auto mb-5 min-h-20 p-5 md:p-2 flex items-center ">
 				<ul className="w-full [&_h4]:uppercase [&_h4]:text-gray-400 [&_h4]:text-xs [&_p]:text-white [&_p]:text-sm flex flex-wrap  *:grow px-2 items-center gap-5 md:justify-around md:divide-x-2 h-full md:[&_li:not(:first-of-type)]:pl-5 *:space-y-1">
@@ -135,7 +135,7 @@ const PassengerDetails = () => {
 					<div className="space-y-8 mt-8">
 						<div className="space-y-5">
 							<h4 className="font-medium text-sm">Passenger 01</h4>
-							<div className="grid gap-3 grid-cols-2 md:grid-cols-3">
+							<div className="grid gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
 								<InputField
 									{...register("passenger1_first_name")}
 									label="First Name"
@@ -172,26 +172,28 @@ const PassengerDetails = () => {
 									handlechange={handleChange}
 								/>
 
-								<InputField
-									{...register("departure_seats")}
-									label="Departure Seat"
-									placeholder="Select departure seat(s)"
-									type="text"
-									readOnly="readonly"
-									value={formData.seatDetails?.departure_seats ?? ""}
-									onClick={handleSeatSelection}
-								/>
-								{formData.bookingDetails.trip_type === "Round Trip" && (
+								<div className="flex gap-5 w-full">
 									<InputField
-										{...register("return_seats")}
-										label="Return Seat"
-										placeholder="Select return seat(s)"
+										{...register("departure_seats")}
+										label="Departure Seat"
+										placeholder="Select departure seat(s)"
 										type="text"
 										readOnly="readonly"
-										value={formData.seatDetails?.return_seats ?? ""}
+										value={formData.seatDetails?.departure_seats ?? ""}
 										onClick={handleSeatSelection}
 									/>
-								)}
+									{formData.bookingDetails.trip_type === "Round Trip" && (
+										<InputField
+											{...register("return_seats")}
+											label="Return Seat"
+											placeholder="Select return seat(s)"
+											type="text"
+											readOnly="readonly"
+											value={formData.seatDetails?.return_seats ?? ""}
+											onClick={handleSeatSelection}
+										/>
+									)}
+								</div>
 							</div>
 						</div>
 						{adults_number > 1 && (
@@ -221,7 +223,7 @@ const PassengerDetails = () => {
 													<h4 className="font-medium text-sm">
 														Passenger 0{i + 2} (Adult)
 													</h4>
-													<div className="grid gap-3 grid-cols-2">
+													<div className="grid gap-3 grid-cols-1 md:grid-cols-2">
 														<InputField
 															{...register(`${currentPassenger}_first_name`, {
 																required: "This field is required.",
