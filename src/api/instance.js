896d6804ee6b87d3d16325891a180ae36d1e11e0
Queen Umesi <queenumesi01@gmail.com>
2024-08-07 04:30:48 +0100

    define env values in vite.config

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,19 +1,14 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.DEV
-	? import.meta.env.VITE_ABITTO_BASE_URL
-	: process.env.ABITTO_BASE_URL;
-
-console.log(import.meta);
+const API_BASE_URL = process.env.ABITTO_BASE_URL;
 
 const baseurl = axios.create({
 	baseURL: API_BASE_URL,
 	headers: {
 		//  Authorization: `<Your Auth Token>`,
 		"Content-Type": "application/json",
-		timeout: 1000,
+		timeout: 10000,
 	},
-	// .. other options
 });
 
 export default baseurl;
